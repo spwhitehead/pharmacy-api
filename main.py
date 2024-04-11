b5a5e94be27a3dc20ba3e5f48933c4eeaf9b80a6
Spencer Whitehead <53270623+spwhitehead@users.noreply.github.com>
2024-04-11 08:59:18 -0600

    Implements all CRUD operations

--- main.py
+++ main.py
@@ -11,9 +11,38 @@ app = FastAPI()
 with open("patients.json", "r") as f:
     patient_list = json.load(f)
 
-patients = list[Patient] = []
+patients: list[Patient] = []
 
-for person in patient_list:
-    patients.append(Patient(**person))
+for patient in patient_list:
+    patients.append(Patient(**patient))
 
 # Use the first name as the unique identifier. For example, in the PUT route, you'd have something like this: "/patients/{first_name}"
+
+
+@app.get("/patients")
+async def list_patients() -> list[Patient]:
+    return patients
+
+
+@app.post("/patients")
+async def add_patient(patient: Patient) -> None:
+    patients.append(patient)
+    return "Patient added successfully"
+
+
+@app.put("/patients/{first_name}")
+async def update_patient(first_name: str, updated_patient: Patient) -> None:
+    for i, patient in enumerate(patients):
+        if patient.first_name == first_name:
+            patients[i] = updated_patient
+            return "Patient updated successfully"
+    return "Patient not found"
+
+
+@app.delete("/patients/{first_name}")
+async def delete_patient(first_name: str) -> None:
+    for i, patient in enumerate(patients):
+        if patient.first_name == first_name:
+            patients.pop(i)
+            return "Patient deleted successfully"
+    return "Patient not found"
--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 
 
 class Patient(BaseModel):
-    f_name: str
-    l_name: str
+    first_name: str
+    last_name: str
     address: str
     age: int
